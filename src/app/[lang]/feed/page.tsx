911b3daf549575b9845dcceb8bc4c853c3cfb3e1
masoudvrz <masoud.varzi@yahoo.com>
2025-07-10 17:51:02 +0330

    feat: created feed page

---
+++ src/app/[lang]/feed/page.tsx
@@ -0,0 +1,19 @@
+
+import FeedList from "@/app/components/FeedList";
+import { getDictionary } from "@/app/lib/dictionaries";
+import { Locale } from "@/app/lib/i18n-config";
+
+export default async function FeedPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const t = await getDictionary(lang);
+
+  return (
+    <main className="p-4 max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">{t.feedTitle}</h1>
+      <FeedList endMessage={t.endMessage} />
+    </main>
+  );
+}
