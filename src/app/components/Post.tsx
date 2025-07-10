be1f59d439f398520e910f03ab4d4c0b8e505e59
masoudvrz <masoud.varzi@yahoo.com>
2025-07-10 19:45:38 +0330

    refactor: updated post component

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,30 +1,29 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { PostType } from '@/app/types/PostType';
+import Image from "next/image";
+import { PostType } from "@/app/types/PostType";
+import { Locale } from "../lib/i18n-config";
+import Link from "next/link";
 
-
-export function Post({ post }: { post: PostType }) {
+export function Post({ post, lang }: { post: PostType; lang: Locale }) {
   return (
     <div className="rounded-2xl shadow-md border border-gray-200 dark:border-gray-700 overflow-hidden ransition-colors duration-300">
-      <div className="relative w-full h-64">
-        <Image
-          src={post.image}
-          alt={post.title}
-          fill
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </div>
+      <Link href={`/${lang}/post/${post.id}`}>
+        <div className="relative w-full h-64">
+          <Image
+            src={post.image}
+            alt={post.title}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
 
-      <div className="p-4 space-y-2">
-        <h2 className="text-xl font-bold">
-          {post.title}
-        </h2>
-        <p className="text-gray-600  text-sm leading-relaxed">
-          {post.body}
-        </p>
-      </div>
+        <div className="p-4 space-y-2">
+          <h2 className="text-xl font-bold">{post.title}</h2>
+          <p className="text-gray-600  text-sm leading-relaxed">{post.body}</p>
+        </div>
+      </Link>
     </div>
   );
 }
